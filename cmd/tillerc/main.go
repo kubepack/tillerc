7c298c97a70406683ac236aacb036560cfba577e
sadlil <sadlil@appscode.com>
2016-12-23 17:47:44 +0600

    added in clusterconfig.

--- cmd/tillerc/main.go
+++ cmd/tillerc/main.go
@@ -8,10 +8,10 @@ import (
 	_ "github.com/appscode/tillerc/api/install"
 	"github.com/appscode/tillerc/pkg/watcher"
 	"github.com/spf13/pflag"
+	"k8s.io/kubernetes/pkg/client/restclient"
 	"k8s.io/kubernetes/pkg/util/flag"
 	"k8s.io/kubernetes/pkg/util/runtime"
 	"k8s.io/kubernetes/pkg/version/verflag"
-	"k8s.io/kubernetes/pkg/client/restclient"
 )
 
 func main() {
@@ -24,20 +24,20 @@ func main() {
 
 	verflag.PrintAndExitIfRequested()
 
-/*	// ref; https://github.com/kubernetes/kubernetes/blob/ba1666fb7b946febecfc836465d22903b687118e/cmd/kube-proxy/app/server.go#L168
-	// Create a Kube Client
-	// define api config source
-	if KubeConfig == "" && Master == "" {
-		log.Println("Neither --kubeconfig nor --master was specified.  Using default API client.  This might not work.")
-	}
-	// This creates a client, first loading any specified kubeconfig
-	// file, and then overriding the Master flag, if non-empty.
-	c, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		&clientcmd.ClientConfigLoadingRules{ExplicitPath: KubeConfig},
-		&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: Master}}).ClientConfig()
-	if err != nil {
-		panic(err)
-	}*/
+	/*	// ref; https://github.com/kubernetes/kubernetes/blob/ba1666fb7b946febecfc836465d22903b687118e/cmd/kube-proxy/app/server.go#L168
+		// Create a Kube Client
+		// define api config source
+		if KubeConfig == "" && Master == "" {
+			log.Println("Neither --kubeconfig nor --master was specified.  Using default API client.  This might not work.")
+		}
+		// This creates a client, first loading any specified kubeconfig
+		// file, and then overriding the Master flag, if non-empty.
+		c, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			&clientcmd.ClientConfigLoadingRules{ExplicitPath: KubeConfig},
+			&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: Master}}).ClientConfig()
+		if err != nil {
+			panic(err)
+		}*/
 
 	c, err := restclient.InClusterConfig()
 	if err != nil {
